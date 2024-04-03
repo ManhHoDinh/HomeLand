835bf393c56b775a4dc34fc2f6db76e830f4f3c2
ngan1808 <20520915@gm.uit.edu.vn>
2024-04-03 08:39:30 +0700

    vehicle

--- src/models/vehicle.ts
+++ src/models/vehicle.ts
@@ -1,1 +1,12 @@
+import { Resident } from "./resident";
+
+export type Vehicle = {
+    id: string ;
+    status:string;
+    licensePlate: string;
+    frontRegistrationPhotoURL:string;
+    backRegistrationPhotoURL:string;
+    licensePlatePhotoURL:string;
+    residentId: string;
+}
 
