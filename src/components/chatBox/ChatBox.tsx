e439a13c156a69963a2b4101cb6a0f8413cd3410
Daiduong1593572468 <daiduong35578@gmail.com>
2024-05-30 21:23:24 +0700

    fix

--- src/components/chatBox/ChatBox.tsx
+++ src/components/chatBox/ChatBox.tsx
@@ -48,39 +48,51 @@ export default function ChatBox(): React.ReactNode {
   const [isAnswering, setIsAnswering] = useState(false);
   function getAnswer(messageList: MessageModel[]) {
     let data = JSON.stringify({
-      input: messageList[messageList.length - 1].message,
-      config: {},
-      kwargs: {},
+      conversation_id: "123",
+      'bot_id': '7374703652214489095',
+      user: "29032201862555",
+      query: messageList[messageList.length - 1].message,
+      stream: false,
     });
-    console.log(messageList[messageList.length - 1].message);
+
     let config = {
       method: "post",
-      maxBodyLength: Infinity,
-      url: "https://phuochungus-chatbot-ai.hf.space/rag-redis/invoke",
+      url: "https://api.coze.com/open_api/v2/chat",
       headers: {
-        accept: "application/json",
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
+        'Accept': '*/*',
+        'Authorization':
+          'Bearer pat_IKoo5BN8jTVO79DBEsKiyaBpVexAC2eDl13v3FS7V3PIQuMO8PHbExQvjnMTtRgC',
+        'Host': 'api.coze.com',
+        'Connection': 'keep-alive',
       },
       data: data,
     };
+
     axios
       .request(config)
       .then((res) => {
         console.log(JSON.stringify(res.data));
         let result = [...messageList];
-        result.push({
-          message: res.data.output,
-          sentTime: new Date().toISOString(),
-          sender: "server",
-          direction: "incoming",
-          position: "first",
-        });
+        const answerMessage = res.data.messages.find((message: { type: string }) => message.type === 'answer');
+        if (answerMessage) {
+          result.push({
+            message: answerMessage.content,
+            sentTime: new Date().toISOString(),
+            sender: "server",
+            direction: "incoming",
+            position: "first",
+          });
+        }
         setMessageList(result);
         setIsAnswering(false);
       })
       .catch((res) => {
         console.log(res);
-      });
+      })
+      .catch((error) => {
+        console.log(error);
+      });;
   }
   useEffect(() => {
     require("@chatscope/chat-ui-kit-styles/dist/default/styles.min.css");
