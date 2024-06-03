d9d96463f4c2e0f774a73b301b098bb710a7f235
phuochungus <21520252@gm.uit.edu.vn>
2024-06-04 00:51:48 +0700

    chore: update model

---
+++ src/models/person.ts
@@ -0,0 +1,10 @@
+export type Person = {
+    id: string,
+    avatar: string,
+    name: string,
+    gender: string,
+    role: string,
+    dateOfBirth: Date,
+    phoneNumber: string,
+    activatedAt: Date,
+}
